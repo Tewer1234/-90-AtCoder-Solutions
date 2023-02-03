c5326240fcffdcace32b14fe799e1204f6985d18
Tewer1234 <108650597+Tewer1234@users.noreply.github.com>
2023-02-03 16:05:05 +0800

    Create 020 - Log Inequality（★3）.cpp

---
+++ 020 - Log Inequality（★3）.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+#include <algorithm>
+#include <bits/stdc++.h>
+#include <iomanip>
+#include <vector>
+#include <map>
+#include <queue>
+#include <stack>
+#include <set>
+#define ll long long int 
+#define pb push_back
+#define inf 0x3f3f3f3f
+#define SIZE 400005
+using namespace std;
+typedef pair<int,int> pp;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  ll a,b,c,t;
+  t=1;
+  cin>>a>>b>>c;
+  for (int i=1;i<=b;i++) t*=c;
+  if (a<t) cout<<"Yes";
+  else cout<<"No";
+  return 0; 
+}
